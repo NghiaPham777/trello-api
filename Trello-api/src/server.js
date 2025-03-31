111160def145fc6fca17f5a8883af56bc29d47f9
nghiaph <nghiapham2704@gmail.com>
2025-03-31 22:57:08 +0700

    cd '/mnt/c/mnt/d/Workspace/Trello-app/trello-api'

--- Trello-api/src/server.js
+++ Trello-api/src/server.js
@@ -6,16 +6,13 @@ import { env } from '~/config/environment'
 
 const START_SERVER = () => {
   const app = express()
-
-  const hostname = 'localhost'
-  const port = 8017
   
   app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1><hr>')
   })
   
   app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`3. Hi ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_HOST}/`)
+    console.log(`3. Hi ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
   //Clean up trước khi stop server
   exitHook(() => {
