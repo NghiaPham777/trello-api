ee824faa8978ac3585ad205039f7f09e26479037
NghiaPham777 <nghiapham2704@gmail.com>
2025-04-14 01:35:14 +0700

    add board service to format title

--- Trello-api/src/controllers/boardController.js
+++ Trello-api/src/controllers/boardController.js
@@ -1,15 +1,20 @@
 import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
+// import ApiError from '~/utils/ApiError'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
     // console.log('request body: ',req.body)
     // console.log('request body: ',req.body)
+    
+    //Dieu huong data sang Service
+    const createdBoard = await boardService.createNew(req.body)
 
     // throw new ApiError(StatusCodes.BAD_GATEWAY, 'test error')
 
     //Response result to Client
-    res.status(StatusCodes.CREATED).json({ message: 'Post from Controller: API create new boards' })
+    res.status(StatusCodes.CREATED).json(createdBoard)
+    // res.status(StatusCodes.CREATED).json({ message: 'POST from Controller'})
   } catch (error) { next(error) }
 }
 
